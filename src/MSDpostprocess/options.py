64d2cb0445bfc3577eb5fcdb07a8b4fc131fa190
Steven Tavis <stavis@vols.utk.edu>
2024-04-04 19:10:05 -0400

    added overwrite checks and working directory setup

--- src/MSDpostprocess/options.py
+++ src/MSDpostprocess/options.py
@@ -11,6 +11,7 @@ class options:
         from argparse import ArgumentParser
         import tomllib
         import os
+        from shutil import copy2
         
         parser = ArgumentParser()
         parser.add_argument('-o', '--options', action = 'store', required = True,
@@ -21,25 +22,47 @@ class options:
             options = tomllib.load(toml)
         self.__dict__.update(options)
         
+        #check that the options toml is valid
         required = ['working_directory',
                     'data',
                     'seed',
                     'min_rt',
                     'lowess_frac',
                     'ppm',
-                    'cutoff_low',
-                    'cutoff_high',
                     'model',
                     'overwrite',
                     'mode',
                     'test_split',
-                    'cores']
+                    # 'cores',
+                    'output']
         problems = [r for r in required if not r in self.__dict__.keys()]
         if problems:
             raise Exception('Required settings not found in options file:\n' + \
                             '\n'.join(problems))
 
+        optfile = os.path.abspath(args.options)
         os.chdir(self.working_directory)
         
-        if self.mode == 'inference' and not os.path.exists(self.model):
+        if self.mode == 'infer' and not os.path.exists(self.model):
             raise Exception('Attempting inference without a pre-trained model.')
+        
+        #prevent overwriting files
+        if not self.overwrite:
+            if self.mode == 'train' and os.path.exists(self.model):
+                raise Exception('Overwrite is false and a model with this name already exists')
+            problems = []
+            for file in ['not_considered.tsv', 
+                         'positive_lipids.tsv', 
+                         'negative_lipids.tsv', 
+                         'reanalyze_lipids.tsv',
+                         os.path.basename(args.options)]:
+                path = os.path.abspath(os.path.join(self.output, file))
+                if os.path.exists(path):
+                    problems.append(path)
+            if problems:
+                raise Exception('Overwrite is false and these files exist:\n' + '\n'.join(problems))
+        
+        if not os.path.exists(self.output):
+            os.mkdir(self.output)
+        copy2(optfile, os.path.join(self.output, os.path.basename(args.options)))
+        
