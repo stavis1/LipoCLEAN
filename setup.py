124759142a9fd51fde17f587a68976c65553d5b9
Steven Tavis <stavis@vols.utk.edu>
2024-05-29 14:00:45 -0400

    added support for installing as a python package

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 28 13:14:54 2024
+
+@author: 4vt
+"""
+
+import setuptools
+
+setuptools.setup(name="MSDpostprocess",
+                 version="0.0.0",
+                 url="https://github.com/stavis1/MSDpostprocess",
+                 author="Steven Tavis",
+                 author_email="stavis@vols.utk.edu",
+                 package_dir={"": "src"},
+                 packages=setuptools.find_namespace_packages(where="src"),
+                 include_package_data=True,
+                 license_files=['LICENSE'],
+                 classifiers=['License :: OSI Approved :: MIT License',
+                              'Operating System :: OS Independent',
+                              'Programming Language :: Python :: 3.11'],
+                 python_requires='==3.11.8',
+                 install_requires=['dill==0.3.8',
+                                   'matplotlib==3.8.4',
+                                   'numpy==1.24.4',
+                                   'pandas==2.2.2',
+                                   'pip==24.0',
+                                   'scikit-learn==1.4.2',
+                                   'scipy==1.13.0',
+                                   'sortedcontainers==2.4.0',
+                                   'statsmodels==0.14.1',
+                                   'brain-isotopic-distribution==1.5.16',
+                                   'linear-tree==0.3.5'])
+
+
+
--- src/MSDpostprocess/__main__.py
+++ src/MSDpostprocess/__main__.py
@@ -7,12 +7,13 @@ Created on Wed Apr  3 10:14:41 2024
 """
 import sys
 
-from options import options, setup_workspace, validate_inputs
-from utilities import read_files, filter_data, split_index, write_data
-from models import mz_correction, rt_correction, predictor_model, add_isotope_error
-from QC import plot_mz_QC, plot_rt_QC, plot_final_QC, plot_pairwise_scores
-
+from MSDpostprocess.options import options, setup_workspace, validate_inputs
 args = options()
+
+from MSDpostprocess.utilities import read_files, filter_data, split_index, write_data
+from MSDpostprocess.models import mz_correction, rt_correction, predictor_model, add_isotope_error
+from MSDpostprocess.QC import plot_mz_QC, plot_rt_QC, plot_final_QC, plot_pairwise_scores
+
 validate_inputs(args)
 setup_workspace(args)
 
