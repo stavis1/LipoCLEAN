50ad3d7fea4ea3a5419c540f4a8fb2d030c9c5a1
Steven Tavis <stavis@vols.utk.edu>
2024-05-30 14:21:05 -0400

    added example analysis to release staging process

--- build/build.sh
+++ build/build.sh
@@ -10,3 +10,8 @@ zip -r release_stage/TOF_model.zip TOF_model/
 
 docker build --file Dockerfile ../ -t stavisvols/msdpostprocess
 docker push stavisvols/msdpostprocess
+
+zip release_stage/example_analysis.zip example_analysis_options.toml ../../data/current_datasets/training_files_with_annotations/QE_MTBLS5583_mzexport_*
+
+gdown --fuzzy https://drive.google.com/drive/folders/1zZ-H4xzIMFjrpgyAtAly2BIeRHeUXgCm --folder
+cp MSDpostprocess/MSDpostprocess.exe results_stage/
