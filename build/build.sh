b20c2a5e0147a4c29f801df0a55a9c2756e22f9b
Steven Tavis <stavis@vols.utk.edu>
2024-06-25 10:36:50 -0400

    bugfix: incorrect capitalization

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 pyinstaller --onefile ../src/lipoCLEAN/__main__.py -n LipoCLEAN --paths ../src/lipoCLEAN/ --add-data ../src/lipoCLEAN/example_options.toml:.
-cp dist/lipoCLEAN release_stage/
+cp dist/LipoCLEAN release_stage/
 
 python -m lipoCLEAN --options build_data/train_QE_Pro_model.toml
 zip -r release_stage/QE_Pro_model.zip QE_Pro_model/
@@ -8,10 +8,10 @@ zip -r release_stage/QE_Pro_model.zip QE_Pro_model/
 python -m lipoCLEAN --options build_data/train_TOF_model.toml
 zip -r release_stage/TOF_model.zip TOF_model/
 
-docker build --file Dockerfile ../ -t stavisvols/lipoCLEAN
-docker push stavisvols/lipoCLEAN
+docker build --file Dockerfile ../ -t stavisvols/lipoclean
+docker push stavisvols/lipoclean
 
 zip release_stage/example_analysis.zip example_analysis_options.toml ../../data/current_datasets/training_files_with_annotations/QE_MTBLS5583_mzexport_*
 
 gdown --fuzzy https://drive.google.com/drive/folders/1zZ-H4xzIMFjrpgyAtAly2BIeRHeUXgCm --folder
-cp lipoCLEAN/lipoCLEAN.exe release_stage/
+cp lipoCLEAN/LipoCLEAN.exe release_stage/
