a1af740777b5ab6abe3901c7cbdc630fb8bb1b04
Steven Tavis <stavis@vols.utk.edu>
2024-10-17 11:14:04 -0400

    updated build resources to accomidate MS-DIAL 5

--- build/build.sh
+++ build/build.sh
@@ -3,13 +3,17 @@
 pyinstaller --onefile ../src/lipoCLEAN/__main__.py -n LipoCLEAN --paths ../src/lipoCLEAN/ --add-data ../src/lipoCLEAN/example_options.txt:.
 cp dist/LipoCLEAN release_stage/
 
-#train QE_Pro_model
-python -m lipoCLEAN --options build_data/train_QE_Pro_model.txt
-zip -r release_stage/QE_Pro_model.zip QE_Pro_model/
+#train MSD4_QE_Pro_model
+python -m lipoCLEAN --options build_data/train_MSD4_QE_Pro_model.txt
+zip -r release_stage/MSD4_QE_Pro_model.zip MSD4_QE_Pro_model/
 
-#train TOF model
-python -m lipoCLEAN --options build_data/train_TOF_model.txt
-zip -r release_stage/TOF_model.zip TOF_model/
+#train MSD5_QE_Pro_model
+python -m lipoCLEAN --options build_data/train_MSD5_QE_Pro_model.txt
+zip -r release_stage/MSD5_QE_Pro_model.zip MSD5_QE_Pro_model/
+
+#train MSD4_TOF_model
+python -m lipoCLEAN --options build_data/train_MSD4_TOF_model.txt
+zip -r release_stage/MSD4_TOF_model.zip MSD4_TOF_model/
 
 #build docker version
 docker build --file Dockerfile ../ -t stavisvols/lipoclean
